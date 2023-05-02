60e84ac659819d296c119f8be467aa5b77ac4d58
LeeGaeul <LeeGaeul@DESKTOP-CQ7DF4E>
2023-05-02 14:28:55 +0900

    초기 커밋.

---
+++ ThreadTest/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ThreadTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Save_btn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Save_btn
+            // 
+            this.Save_btn.Location = new System.Drawing.Point(283, 286);
+            this.Save_btn.Name = "Save_btn";
+            this.Save_btn.Size = new System.Drawing.Size(75, 23);
+            this.Save_btn.TabIndex = 1;
+            this.Save_btn.Text = "Save File";
+            this.Save_btn.UseVisualStyleBackColor = true;
+            this.Save_btn.Click += new System.EventHandler(this.Save_btn_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(14, 254);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(344, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 63);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 177);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 10F);
+            this.label1.Location = new System.Drawing.Point(40, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 14);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Title :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 10F);
+            this.label2.Location = new System.Drawing.Point(11, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 14);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Content :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 284);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 317);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Save_btn);
+            this.Name = "Form1";
+            this.Text = "ThreadTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Save_btn;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ThreadTest/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace ThreadTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 윈도우 폼에서의 다중 스레드
+        // 크로스 스레드 발생 -> delegate와 invoke에 대해 알아야 함
+        private void Save_btn_Click(object sender, EventArgs e)
+        {
+            // 텍스트 입력 유효성 확인
+            if (textBox2.Text.Equals("") || textBox2 == null)
+            {
+                MessageBox.Show("저장할 파일명을 적어주세요.");
+                return;
+            }
+            if (textBox1.Text.Equals("") || textBox1 == null)
+            {
+                MessageBox.Show("저장할 내용을 적어주세요.");
+                return;
+            }
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+
+            // 스레드 호출(파일 저장 및 진행바)
+            Thread saveFileThr = new Thread(saveFile);
+            Thread pBarThr = new Thread(pBar);
+
+            saveFileThr.Start();
+            pBarThr.Start();
+
+            // 스레드 수행 완료 후 메인 스레드와 합류
+            pBarThr.Join();
+            saveFileThr.Join();
+            MessageBox.Show("파일 저장 완료.");
+
+            // textbox, progress bar reset
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            label3.Text = string.Empty;
+            progressBar1.Value = 0;
+        }
+
+        private void saveFile() // 여기에는 왜 델리게이트와 invoke가 필요 없는가? 컨트롤을 생성하지 않았기 때문
+        {
+            StreamWriter writer;
+            writer = File.CreateText(@"C:\Users\LeeGaeul\Desktop\" + textBox2.Text + ".txt");
+            //Text File이 저장될 위치(파일명), 파일 이름만 지정하면 컴파일된 폴더내 해당 파일 이름으로 저장됨
+            writer.WriteLine(textBox1.Text); // 저장될 string
+            writer.Close();
+        }
+
+        private void pBar()
+        {
+            for (int i=0; i<progressBar1.Maximum; i++)
+            {
+                SetProgressValue(i);
+            /*
+                progressBar1.Value = var;
+                label3.Text = ((int)var + 1).ToString() + "%";
+            */
+                Thread.Sleep(5); // 진행바 속도 조절
+            }
+        }
+
+        delegate void pBarDel(int var); // 대리자를 쓰는 이유가 무엇인가?
+        private void SetProgressValue(int var) // 이 과정이 정말 이해가 안간다..
+        {
+            // InvokeRequired => 크로스 스레드 발생 원인의 값을 구분 지을 수 있음
+            // true이면, 현재 스레드가 자신을 생성한 스레드가 아닌 것
+            // false이면, 현재 스레드가 자신을 생성한 스레드인 것
+            if (progressBar1.InvokeRequired)
+            {
+                pBarDel del = new pBarDel(SetProgressValue); // 대리자 객체 생성
+                progressBar1.BeginInvoke(del, var);
+            }
+            else
+            {
+                progressBar1.Value = var;
+                label3.Text = ((int)var + 1).ToString() + "%";
+            }       
+        }
+
+        
+    }
+}
